8903aa30515cf6c84f1e20a26f4e518c7b4899d9
Nida-Gonul <138121191+Nida-Gonul@users.noreply.github.com>
2023-09-15 13:34:51 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+function linkedin() {
+  window.open("https://www.linkedin.com/in/nida-gonul-851b1957")
+}
